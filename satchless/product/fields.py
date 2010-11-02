dc2eb7213467f3081b172d7e34e324d7c599b10c
Patryk Zawadzki <patrys@pld-linux.org>
2010-11-02 18:27:17 +0100

    More queries, more art

--- satchless/product/fields.py
+++ satchless/product/fields.py
@@ -6,8 +6,12 @@ class CategoryMixin:
     def label_from_instance(self, obj):
         level = getattr(obj, obj._mptt_meta.level_attr)
         indent = max(0, level - 1) * u'│'
-        if level:
-            indent += u'├ '
+        if obj.parent:
+            last = obj.parent.get_children().order_by('-rght')[0]
+            if last == obj and not obj.get_children().exists():
+                indent += u'└ '
+            else:
+                indent += u'├ '
         return u'%s%s' % (indent, unicode(obj))
 
 class CategoryChoiceField(CategoryMixin, forms.ModelChoiceField):

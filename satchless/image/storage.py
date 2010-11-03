e03d29c2e3e9b4a69f5cf05df5afa10b765d570d
Patryk Zawadzki <patrys@pld-linux.org>
2010-11-03 18:09:42 +0100

    Clean up

--- satchless/image/storage.py
+++ satchless/image/storage.py
@@ -16,10 +16,4 @@ class HashedStorage(FileSystemStorage):
         directory, file_name = os.path.split(name)
         name = os.path.join(directory, 'by-md5', hash[:1], hash[1:2], hash, file_name)
 
-#        try:
-#            os.makedirs(os.path.join(settings.MEDIA_ROOT, directory))
-#        except OSError as e:
-#            if e.errno is not errno.EEXIST:
-#                raise e
-
         return super(HashedStorage, self).save(name, content)

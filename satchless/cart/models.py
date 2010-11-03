5a247a1132a8f471d1f64636ea5ce7c622286ae6
Michał Sałaban <michal@salaban.info>
2010-11-03 17:27:25 +0100

    Cart model with basic functionality

---
+++ satchless/cart/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+from satchless.product.models import Variant
+
+from decimal import Decimal
+
+CART_SESSION_KEY = '_satchless_cart-%s' # takes typ
+
+class CartManager(models.Manager):
+    def get_or_create_from_request(request, typ):
+        try:
+            return self.get(typ=typ, pk=request.session[CART_SESSION_KEY % typ])
+        except Cart.DoesNotExist:
+            owner = request.user if request.user.is_authenticated() else None
+            cart = self.create(typ=typ, owner=owner)
+            request.session[CART_SESSION_KEY % typ] = cart.pk
+            return cart
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey(User, null=True, blank=True)
+    typ = models.CharField(_("type"), max_length=100)
+
+    objects = CartManager()
+
+    def set_quantity(self, variant, quantity):
+        try:
+            item = self.items.get(variant=variant)
+            if quantity > 0:
+                item.quantity = quantity
+                item.save()
+            else:
+                item.delete()
+        except CartItem.DoesNotExist:
+            if quantity > 0:
+                item = self.items.create(variant=variant, quantity=quantity)
+
+    def get_quantity(self, variant):
+        try:
+            return self.items.get(variant=variant).quantity
+        except CartItem.DoesNotExist:
+            return Decimal('0')
+
+    def __unicode__(self):
+        if self.owner:
+            return u"%s of %s" % (self.typ, self.user.username)
+        else:
+            return self.typ
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items')
+    variant = models.ForeignKey(Variant)
+    quantity = models.DecimalField(_("quantity"), max_digits=10, decimal_places=4)
+
+    def __unicode__(self):
+        return u"%s x %s" % (self.variant, self.quantity)
+
+    class Meta:
+        unique_together = ('cart', 'variant')
---
+++ satchless/cart/tests.py
@@ -0,0 +1,49 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+from django.db import models
+from satchless.product.models import ProductAbstract
+from .models import *
+
+from decimal import Decimal
+
+class DeadParrot(ProductAbstract):
+    species = models.CharField(max_length=20)
+
+class DeadParrotVariant(Variant):
+    product = models.ForeignKey(DeadParrot, related_name='variants')
+    color = models.CharField(max_length=10, choices=(('blue', 'blue'), ('white', 'white')))
+    looks_alive = models.BooleanField()
+
+    class Meta:
+        unique_together = ('product', 'color', 'looks_alive')
+
+
+class ParrotTest(TestCase):
+    def setUp(self):
+        self.macaw = DeadParrot.objects.create(slug='macaw',
+                species="Hyacinth Macaw")
+        self.cockatoo = DeadParrot.objects.create(slug='cockatoo',
+                species="White Cockatoo")
+        self.macaw_blue = self.macaw.variants.create(color='blue', looks_alive=False)
+        self.macaw_blue_fake = self.macaw.variants.create(color='blue', looks_alive=True)
+        self.cockatoo_white_a = self.cockatoo.variants.create(color='white', looks_alive=True)
+        self.cockatoo_white_d = self.cockatoo.variants.create(color='white', looks_alive=False)
+        self.cockatoo_blue_a = self.cockatoo.variants.create(color='blue', looks_alive=True)
+        self.cockatoo_blue_d = self.cockatoo.variants.create(color='blue', looks_alive=False)
+
+    def test_basic_cart_ops(self):
+        cart = Cart.objects.create(typ='satchless.test_cart')
+        cart.set_quantity(self.macaw_blue, 1)
+        cart.set_quantity(self.macaw_blue_fake, Decimal('2.45'))
+        cart.set_quantity(self.cockatoo_white_a, Decimal('2.45'))
+        cart.set_quantity(self.cockatoo_white_d, 4.11)
+        cart.set_quantity(self.cockatoo_blue_a, 6)
+        cart.set_quantity(self.cockatoo_blue_d, Decimal('2'))
+        cart.set_quantity(self.cockatoo_white_d, 0) # throw out
+        self.assertEqual(cart.get_quantity(self.macaw_blue), Decimal('1'))
+        self.assertEqual(cart.get_quantity(self.macaw_blue_fake), Decimal('2.45'))
+        self.assertEqual(cart.get_quantity(self.cockatoo_white_a), Decimal('2.45'))
+        self.assertEqual(cart.get_quantity(self.cockatoo_white_d), Decimal('0')) # thrown out
+        self.assertRaises(CartItem.DoesNotExist, cart.items.get, variant=self.cockatoo_white_d)
+        self.assertEqual(cart.get_quantity(self.cockatoo_blue_a), Decimal('6'))
+        self.assertEqual(cart.get_quantity(self.cockatoo_blue_d), Decimal('2'))

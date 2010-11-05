50f4bb6d80978998fb92abd32e5f2bbb1c9ac167
Michał Sałaban <michal@salaban.info>
2010-11-05 14:38:51 +0100

    Default quantity is 1

--- satchless/cart/forms.py
+++ satchless/cart/forms.py
@@ -16,7 +16,7 @@ class AddToCartForm(forms.Form, QuantityForm):
     """Form that adds a Variant quantity to a Cart.
     It may be replaced by more advanced one, performing some checks, e.g.
     verifying the number of items in stock."""
-    quantity = forms.DecimalField()
+    quantity = forms.DecimalField(initial=1)
     typ = forms.CharField(max_length=100, widget=forms.HiddenInput())
 
     def __init__(self, data=None, *args, **kwargs):

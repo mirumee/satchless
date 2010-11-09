5861e581aa0c9eee3e1f12e1d95f53225015b8c1
Michał Sałaban <michal@salaban.info>
2010-11-09 15:33:38 +0100

    Simplified

--- satchless/cart/urls.py
+++ satchless/cart/urls.py
@@ -4,13 +4,9 @@ from . import forms
 
 urlpatterns = patterns('',
         # TODO
-        # After http://code.djangoproject.com/ticket/13154 has been fixed, uncomment
-        # the following lines:
-        #
-        #url(r'^view/$', views.cart, {'typ': 'satchless_cart'}, name='satchless-cart-view'),
-        #
-        # ...and remove these:
+        # After http://code.djangoproject.com/ticket/13154 has been fixed, remove
+        # the following line:
         url(r'^view/(?P<typ>satchless_cart)/$', views.cart, name='satchless-cart-view'),
-        url(r'^view/$', views.cart, {'typ': 'satchless_cart'}, name='satchless-cart-view'),
         # ...stop removing here.
+        url(r'^view/$', views.cart, {'typ': 'satchless_cart'}, name='satchless-cart-view'),
         )

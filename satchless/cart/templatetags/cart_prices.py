8519d14b1362d018b440b62033290b4fa979a9dc
Michał Sałaban <michal@salaban.info>
2010-11-17 22:47:34 +0100

    Fixing tags

--- satchless/cart/templatetags/cart_prices.py
+++ satchless/cart/templatetags/cart_prices.py
@@ -5,22 +5,19 @@ register = template.Library()
 @register.filter
 def cartitem_unit_price(cartitem):
     try:
-        from satchless.pricing import get_handler
-        return get_handler().get_cartitem_unit_price(cartitem)
-    except (ImportError, NotImplementedError):
+        from satchless.pricing import get_cartitem_unit_price
+        return get_cartitem_unit_price(cartitem)
+    except ImportError:
         return ''
 
 @register.filter
 def cartitem_price(cartitem):
     try:
-        from satchless.pricing import get_handler
-        return get_handler().get_cartitem_unit_price(cartitem)
+        from satchless.pricing import get_cartitem_unit_price
+        return get_cartitem_unit_price(cartitem)
     except (ImportError, NotImplementedError):
         return ''
 
 @register.filter
 def cart_total(cart):
-    try:
-        return cart.get_total_price()
-    except (ImportError, NotImplementedError):
-        return ''
+    return cart.get_total_price()
--- satchless/product/templatetags/product_prices.py
+++ satchless/product/templatetags/product_prices.py
@@ -5,15 +5,15 @@ register = template.Library()
 @register.filter
 def variant_price(variant):
     try:
-        from satchless.pricing import get_handler
-        return get_handler().get_variant_price(variant=variant.get_subtype_instance())
-    except (ImportError, NotImplementedError)
+        from satchless.pricing import get_variant_price
+        return get_variant_price(variant.get_subtype_instance())
+    except ImportError:
         return ''
 
 @register.filter
 def product_price_range(product):
     try:
-        from satchless.pricing import get_handler
-        return get_handler()(product=product.get_subtype_instance())
-    except PriceException:
+        from satchless.pricing import get_product_price_range
+        return get_product_price_range(product.get_subtype_instance())
+    except ImportError:
         return ''

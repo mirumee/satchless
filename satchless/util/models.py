5ff3ae59a283b7a49233a7eca41799b380927776
Patryk Zawadzki <patrys@pld-linux.org>
2011-02-02 12:50:04 +0100

    Lint

--- satchless/util/models.py
+++ satchless/util/models.py
@@ -2,22 +2,13 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 class Subtyped(models.Model):
-    content_type = models.ForeignKey(ContentType, editable=False)
+    content_type = models.ForeignKey(ContentType, editable=False,
+                                     related_name='+')
     _subtype_instance = None
     __in_unicode = False
 
-    def get_subtype_instance(self, refresh=False):
-        """
-        Caches and returns the final subtype instance. If refresh is set,
-        the instance is taken from database, no matter if cached copy
-        exists.
-        """
-        if not self._subtype_instance or refresh:
-            self._subtype_instance = self.content_type.get_object_for_this_type(pk=self.pk)
-        return self._subtype_instance
-
-    def store_content_type(self, klass):
-        self.content_type = ContentType.objects.get_for_model(klass)
+    class Meta:
+        abstract = True
 
     def __unicode__(self):
         # XXX: can we do it in more clean way?
@@ -31,8 +22,19 @@ class Subtyped(models.Model):
         else:
             return self.get_subtype_instance().__unicode__()
 
-    class Meta:
-        abstract = True
+    def get_subtype_instance(self, refresh=False):
+        """
+        Caches and returns the final subtype instance. If refresh is set,
+        the instance is taken from database, no matter if cached copy
+        exists.
+        """
+        if not self._subtype_instance or refresh:
+            instance = self.content_type.get_object_for_this_type(pk=self.pk)
+            self._subtype_instance = instance
+        return self._subtype_instance
+
+    def store_content_type(self, klass):
+        self.content_type = ContentType.objects.get_for_model(klass)
 
 def _store_content_type(sender, instance, **kwargs):
     if isinstance(instance, Subtyped):

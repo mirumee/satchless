9c4c67eab702558feb51efff6a0685507cad0b6a
Michał Sałaban <michal@salaban.info>
2011-02-18 18:47:01 +0100

    Rem debóg print

--- satchless/order/views.py
+++ satchless/order/views.py
@@ -7,7 +7,6 @@ from . import forms
 def checkout(request, typ):
     cart = Cart.objects.get_or_create_from_request(request, typ)
     order = models.Order.objects.create_for_cart(cart)
-    print order.groups.all()
     delivery_formset = forms.DeliveryMethodFormset(queryset=order.groups.all())
     return direct_to_template(request, 'satchless/order/checkout.html',
             {'order': order, 'delivery_formset': delivery_formset})

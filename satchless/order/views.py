005fa4d91d79a8533015d4d04cd51eff0e36e3f0
Michał Sałaban <michal@salaban.info>
2011-03-01 10:32:14 +0100

    Stubs and inline docs

--- satchless/order/views.py
+++ satchless/order/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
 from satchless.cart.models import Cart
@@ -7,6 +8,11 @@ from . import forms
 from . import handler
 
 def checkout(request, typ):
+    """
+    Checkout step 1
+    The order is split into delivery groups. User chooses delivery method
+    for each of the groups.
+    """
     cart = Cart.objects.get_or_create_from_request(request, typ)
     order = models.Order.objects.create_for_cart(cart, session=request.session)
     delivery_formset = forms.DeliveryMethodFormset(data=request.POST or None, queryset=order.groups.all())
@@ -18,6 +24,11 @@ def checkout(request, typ):
             {'order': order, 'delivery_formset': delivery_formset})
 
 def delivery_details(request):
+    """
+    Checkout step 1½
+    If there are any delivery details needed (e.g. the shipping address),
+    user will be asked for them. Otherwise we redirect to step 2.
+    """
     order = models.Order.objects.get_from_session(request.session)
     delivery_groups_forms = forms.get_delivery_details_forms_for_groups(order, request)
     groups_with_forms = filter(lambda gf: gf[2], delivery_groups_forms)
@@ -38,7 +49,27 @@ def delivery_details(request):
             {'order': order, 'delivery_groups_forms': groups_with_forms})
 
 def payment_choice(request):
+    """
+    Checkout step 2
+    User will choose the payment method.
+    """
     order = models.Order.objects.get_from_session(request.session)
     payment_form = forms.PaymentMethodForm(data=request.POST or None, instance=order)
     return direct_to_template(request, 'satchless/order/payment_choice.html',
             {'order': order, 'payment_form': payment_form})
+
+def payment_details(request):
+    """
+    Checkout step 2½
+    If any payment details are needed, user will be asked for them. Otherwise
+    we redirect to step 3.
+    """
+    pass
+
+def confirmation(request):
+    """
+    Checkout step 3
+    The final summary, where user is asked to review and confirm the order.
+    Confirmation will redirect to the payment gateway.
+    """
+    pass

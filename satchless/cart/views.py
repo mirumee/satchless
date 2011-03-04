a5fc8514bf65e4e423ca6c1c696d89456aba0ffe
Tomasz Rybarczyk <paluho@gmail.com>
2011-03-04 16:47:10 +0100

    Cart view accepts custom item form.

--- satchless/cart/views.py
+++ satchless/cart/views.py
@@ -5,15 +5,15 @@ from django.template import RequestContext
 from . import models
 from . import forms
 
-def cart(request, typ):
+def cart(request, typ, formset_class=forms.CartItemFormSet):
     cart = models.Cart.objects.get_or_create_from_request(request, typ)
     if request.method == 'POST':
-        formset = forms.CartItemFormSet(instance=cart, data=request.POST)
+        formset = formset_class(instance=cart, data=request.POST)
         if formset.is_valid():
             formset.save()
             return HttpResponseRedirect(reverse('satchless-cart-view', kwargs={'typ': typ}))
     else:
-        formset = forms.CartItemFormSet(instance=cart)
+        formset = formset_class(instance=cart)
     return render_to_response(
             ['satchless/cart/%s/view.html' % typ, 'satchless/cart/view.html'],
             {'cart': cart, 'formset': formset},

f0374b5d6e40e3bae5a7190bc26a60811f15873c
Tomasz Rybarczyk <paluho@gmail.com>
2011-03-16 18:51:39 +0100

    Cosmetics - s/search_product/search_products/.

--- satchless/contrib/search/haystack_predictive/urls.py
+++ satchless/contrib/search/haystack_predictive/urls.py
@@ -5,5 +5,5 @@ from . import forms
 from . import views
 
 urlpatterns = patterns('haystack.views',
-    url(r'^$', views.search_product, name='satchless-search-haystack-predictive'),
+    url(r'^$', views.search_products, name='satchless-search-haystack-predictive'),
 )

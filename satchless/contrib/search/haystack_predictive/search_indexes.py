a5deccdf3dd894af9b5d7cdf1627a606773dacd6
Tomasz Rybarczyk <paluho@gmail.com>
2011-03-16 13:30:26 +0100

    Simple predictive search app based on haystack - initial version.

---
+++ satchless/contrib/search/haystack_predictive/search_indexes.py
@@ -0,0 +1,31 @@
+import datetime
+
+from django.conf import settings
+
+from haystack import site
+from haystack.indexes import *
+
+from satchless.product import models
+
+# Because haystack allows registering only one search index per model
+# all translations are kept in one dynamically created index
+
+class ProductEdgeNgramField(EdgeNgramField):
+    def __init__(self, fieldname, *args, **kwargs):
+        self.fieldname = fieldname
+        super(ProductEdgeNgramField, self).__init__(*args, **kwargs)
+
+    def prepare(self, obj):
+        product = obj.get_subtype_instance()
+        translations = [ getattr(product, self.fieldname) ]
+        for product_translation in product.translations.all():
+            translations.append(getattr(product_translation, self.fieldname))
+        return ' '.join(translations)
+
+class ProductPredictiveSearchIndex(SearchIndex):
+    text = ProductEdgeNgramField(fieldname='name', document=True)
+
+    def get_queryset(self):
+        return models.Product.objects.all()
+
+site.register(models.Product, ProductPredictiveSearchIndex)
---
+++ satchless/contrib/search/haystack_predictive/views.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+from django.conf import settings
+
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, InvalidPage
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import get_object_or_404
+from django.views.generic.simple import direct_to_template
+
+from haystack.query import RelatedSearchQuerySet
+
+from satchless.product.models import Product
+
+from . import forms
+
+PRODUCTS_PER_PAGE = 5
+
+def search_product(request, template_name='satchless/search/haystack_predictive/products.html'):
+    form = forms.ProductPredictiveSearchForm(data=request.GET or None)
+    if form.is_valid():
+        results = form.search()
+        query = form.cleaned_data['q']
+    else:
+        results = RelatedSearchQuerySet().all()
+        results = results.load_all()
+        results = results.models(Product)
+        query = ''
+
+    products_per_page = getattr(settings, 'HAYSTACK_PREDICTIVE_PRODUCTS_PER_PAGE', PRODUCTS_PER_PAGE)
+    paginator = Paginator(results, products_per_page)
+    try:
+        page = paginator.page(request.GET.get('page', 1))
+    except InvalidPage:
+        raise Http404()
+
+    return direct_to_template(request, template_name, {
+        'form': form,
+        'page': page,
+        'paginator': paginator,
+        'query': query,
+    })
+

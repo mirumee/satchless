6ab80cad824b2885911935b0a69c8f56d262d12b
Michał Sałaban <michal@salaban.info>
2011-03-16 20:42:44 +0100

    Added handler for mamona

---
+++ satchless/contrib/payment/mamona_provider/models.py
@@ -0,0 +1,25 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from mamona.models import build_payment_model
+from satchless.payment.models import PaymentVariant
+
+class MamonaPaymentVariant(PaymentVariant):
+    def __unicode__(self):
+        try:
+            payment = self.payments.get()
+        except ObjectDoesNotExist:
+            return u"(empty mamona payment)"
+        if payment.status == 'paid':
+            return u"%.2f %s via %s on %s (by mamona)" % (
+                    payment.amount,
+                    payment.get_status_display(),
+                    payment.backend,
+                    payment.paid_on)
+        else:
+            return u"%.2f %s via %s created on %s (by mamona)" % (
+                    payment.amount,
+                    payment.get_status_display(),
+                    payment.backend,
+                    payment.created_on)
+
+Payment = build_payment_model(MamonaPaymentVariant, unique=True, related_name='payments')

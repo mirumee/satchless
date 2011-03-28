256afa1086681e09b4de1daa4ffc4c82de6c9b62
Michał Sałaban <michal@salaban.info>
2011-03-28 18:07:17 +0200

    Be lazy, translation

--- satchless/util/countries.py
+++ satchless/util/countries.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 COUNTRY_CHOICES = (
     ('AF', _(u'Afghanistan')),

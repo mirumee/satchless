43fcdb228ed59935390ec0cec6d1d7a4e9ada0b8
Allan Davis <javaalley@gmail.com>
2011-04-18 21:46:51 +0800

    Inital start of setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+from distutils.core import setup
+
+# dynamic retrive version number from stachless.VERSION
+version_tuple = __import__('satchless').VERSION
+version = ".".join([str(v) for v in version_tuple])
+
+setup(
+	name="satchless",
+	author="Mirumee Labs",
+	author_email="hello@mirumee.com",
+	description=""" e-commerence framework for creating storefronts in django""",
+	version = version,
+	url = "http://satchless.com/"
+	packages = ["satchless",],
+)

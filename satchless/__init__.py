014d86c8d2ccb95cbea8029771af25dc80cce258
Patryk Zawadzki <patrys@pld-linux.org>
2011-04-21 12:26:20 +0200

    Use dates for versioning, update setup.py

--- satchless/__init__.py
+++ satchless/__init__.py
@@ -4,4 +4,4 @@ Satchless
 The Django e-commerce framework for perfectionists who also care about code
 quality.
 '''
-VERSION = (0, 1, 'pre')
+VERSION = (2011, 4, 'pre')
--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ from distutils.core import setup
 version_tuple = __import__('satchless').VERSION
 version = ".".join([str(v) for v in version_tuple])
 
-setup(
-	name="satchless",
-	author="Mirumee Labs",
-	author_email="hello@mirumee.com",
-	description=""" e-commerence framework for creating storefronts in django""",
-	version = version,
-	url = "http://satchless.com/",
-	packages = ["satchless",],
-)
+setup(name="satchless",
+      author="Mirumee Software",
+      author_email="hello@mirumee.com",
+      description="""An e-commerence framework for Django""",
+      version = version,
+      url = "http://satchless.com/",
+      packages = ["satchless",],
+     )

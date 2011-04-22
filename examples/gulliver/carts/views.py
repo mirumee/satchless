2ac6c5c012c9a34ff7976c2e4fbc17e3fa4b9ace
Tomasz Rybarczyk <paluho@gmail.com>
2011-04-22 15:09:39 +0200

    Demo shop: fix in wishlist view

--- examples/gulliver/carts/views.py
+++ examples/gulliver/carts/views.py
@@ -8,10 +8,10 @@ from django.utils.translation import ugettext_lazy as _
 from satchless.cart.models import Cart
 
 def from_wishlist_to_cart(request, wishlist_item_id):
-    wishlist = Cart.objects.get_from_request(request, 'satchless_wishlist')
+    wishlist = Cart.objects.get_or_create_from_request(request, 'satchless_wishlist')
     item = get_object_or_404(wishlist.items.all(), id=wishlist_item_id)
 
-    cart = Cart.objects.get_from_request(request, 'satchless_cart')
+    cart = Cart.objects.get_or_create_from_request(request, 'satchless_cart')
     cart.add_quantity(variant=item.variant, quantity=1)
     messages.success(request, _(u'Product added to cart.'))
 

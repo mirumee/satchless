c667d0fd475da213e7c2f51aa6a9af6476151a66
Tomasz Rybarczyk <paluho@gmail.com>
2011-04-22 14:11:05 +0200

    Demo shop: simple tag for variant attrs

---
+++ examples/gulliver/products/templatetags/products_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+from django.utils.translation import ugettext as _
+
+from .. import models
+
+register = template.Library()
+
+@register.filter
+def variant_attrs(variant):
+    if isinstance(variant, models.HatVariant):
+        return ()
+    return ({'name':_('Size'), 'value': variant.get_size_display()},
+            {'name':_('Color'), 'value': variant.get_color_display()})

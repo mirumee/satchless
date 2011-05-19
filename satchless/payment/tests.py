ed65bafeacb71a2149c89dbda54d36b8502e5ff7
Patryk Zawadzki <patrys@pld-linux.org>
2011-05-19 16:53:53 +0200

    Provide fields for CC handling

--- satchless/payment/tests.py
+++ satchless/payment/tests.py
@@ -5,19 +5,60 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
+import datetime
+from django import forms
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from . import fields
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class CreditCardNumberFieldTest(TestCase):
+    def setUp(self):
+        self.field = fields.CreditCardNumberField()
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_valid_number(self):
+        TEST_NUMBERS = [
+            # VISA
+            '4111 1111 1111 1111',
+            # MasterCard
+            '5500 0000 0000 0004',
+            '5424 0000 0000 0015',
+            # American Express
+            '3782 8224 6310 005',
+            '3400 0000 0000 009',
+            '3700 0000 0000 002',
+            # Discover
+            '6011 1111 1111 1117',
+            '6011 0000 0000 0004',
+            '6011 0000 0000 0012',
+            # Diner's Club
+            '3000 0000 0000 04',
+            '2014 0000 0000 009',
+            # JCB
+            '3088 0000 0000 0009',
+        ]
+        for number in TEST_NUMBERS:
+            self.assertTrue(self.field.clean(number))
 
+    def test_invalid_number(self):
+        INVALID_NUMBERS = [
+            '9999 9999 9999 9999',
+            'abc',
+            '--',
+        ]
+        for number in INVALID_NUMBERS:
+            self.assertRaises(forms.ValidationError, self.field.clean,
+                              number)
+
+
+class CreditCardExpirationFieldTest(TestCase):
+    def setUp(self):
+        self.field = fields.CreditCardExpirationField()
+
+    def test_valid_date(self):
+        expires = datetime.date.today() + datetime.timedelta(days=30)
+        self.assertTrue(self.field.clean(expires))
+
+    def test_past_date(self):
+        expires = datetime.date.today() + datetime.timedelta(days=-60)
+        self.assertRaises(forms.ValidationError, self.field.clean,
+                          expires)

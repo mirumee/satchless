4e3e05e155eb83fab6f500120bb8023cf8f1bf89
Patryk Zawadzki <patrys@pld-linux.org>
2011-05-20 19:22:43 +0200

    Split name for physical shipping

--- satchless/contrib/delivery/simplepost/forms.py
+++ satchless/contrib/delivery/simplepost/forms.py
@@ -5,7 +5,7 @@ from . import models
 class PostShippingVariantForm(forms.ModelForm):
     class Meta:
         model = models.PostShippingVariant
-        fields = ('shipping_full_name', 'shipping_company_name',
-            'shipping_street_address_1', 'shipping_street_address_2',
-            'shipping_city', 'shipping_postal_code', 'shipping_country',
-            'shipping_phone')
+        fields = ('shipping_first_name', 'shipping_last_name',
+                  'shipping_company_name', 'shipping_street_address_1',
+                  'shipping_street_address_2', 'shipping_city',
+                  'shipping_postal_code', 'shipping_country', 'shipping_phone')
--- satchless/delivery/models.py
+++ satchless/delivery/models.py
@@ -21,7 +21,8 @@ class DeliveryVariant(Subtyped):
 
 
 class PhysicalShippingVariant(DeliveryVariant):
-    shipping_full_name = models.CharField(_("full person name"), max_length=256)
+    shipping_first_name = models.CharField(_("first name"), max_length=256)
+    shipping_last_name = models.CharField(_("last name"), max_length=256)
     shipping_company_name = models.CharField(_("company name"),
                                              max_length=256, blank=True)
     shipping_street_address_1 = models.CharField(_("street address 1"),
@@ -34,7 +35,7 @@ class PhysicalShippingVariant(DeliveryVariant):
                                         choices=countries.COUNTRY_CHOICES,
                                         max_length=2, blank=True)
     shipping_country_area = models.CharField(_("country administrative area"),
-                                            max_length=128, blank=True)
+                                             max_length=128, blank=True)
     shipping_phone = models.CharField(_("phone number"),
                                       max_length=30, blank=True)
 
@@ -42,4 +43,9 @@ class PhysicalShippingVariant(DeliveryVariant):
         abstract = True
 
     def __unicode__(self):
-        return "%s for %s, %s" % (self.name, self.shipping_full_name, self.get_shipping_country_display())
+        return "%s for %s, %s" % (self.name, self.shipping_full_name,
+                                  self.get_shipping_country_display())
+
+    @property
+    def shipping_full_name(self):
+        return u'%s %s' % (self.shipping_first_name, self.shipping_last_name)

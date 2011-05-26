01a22fd0fe446aa5d8cbf6ad0822ae5838064de6
Tomasz Rybarczyk <paluho@gmail.com>
2011-05-27 00:44:12 +0200

    Demo shop: fix in products templatetag

--- examples/demo/products/templatetags/products_tags.py
+++ examples/demo/products/templatetags/products_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 
 @register.filter
 def variant_attrs(variant):
-    if isinstance(variant, models.HatVariant):
+    if isinstance(variant, models.HatVariant) or not hasattr(variant, 'size') or not hasattr(variant, 'color'):
         return ()
     return ({'name':_('Size'), 'value': variant.get_size_display()},
             {'name':_('Color'), 'value': variant.get_color_display()})

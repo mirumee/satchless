aa646d30d2e6f9a96b615e1be4a96aef10fcedf4
Tomasz Rybarczyk <paluho@gmail.com>
2011-05-31 15:47:25 +0200

    Order view url pattern fixes

--- satchless/order/tests.py
+++ satchless/order/tests.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from decimal import Decimal
+
+from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 
 from satchless.product.models import Category
@@ -40,3 +42,12 @@ class OrderTest(TestCase):
             order_items.update(group.items.values_list('product_variant', 'quantity'))
         self.assertEqual(set(cart.items.values_list('variant', 'quantity')), order_items)
 
+    def test_order_view(self):
+        cart = Cart.objects.create(typ='satchless.test_cart')
+        cart.set_quantity(self.macaw_blue, 1)
+        cart.set_quantity(self.macaw_blue_fake, Decimal('2.45'))
+        cart.set_quantity(self.cockatoo_white_a, Decimal('2.45'))
+
+        order = models.Order.objects.get_from_cart(cart)
+        response = self.client.get(reverse('satchless-order-view', args=(order.token,)))
+        self.assertEqual(response.status_code, 200)
--- satchless/order/urls.py
+++ satchless/order/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^my-orders/$', views.my_orders, name='satchless-order-my-orders'),
-    url(r'^(?P<order_pk>[0-9]+)/$', views.view, name='satchless-order-view'),
+    url(r'^(?P<order_token>[0-9a-zA-Z]+)/$', views.view, name='satchless-order-view'),
     )

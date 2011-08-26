78be49e00d7d828c4bc5d4286282151c14864fbd
Patryk Zawadzki <patrys@pld-linux.org>
2011-08-26 23:56:20 +0200

    List excludes rather than fields

--- satchless/contrib/delivery/simplepost/forms.py
+++ satchless/contrib/delivery/simplepost/forms.py
@@ -5,7 +5,4 @@ from . import models
 class PostShippingVariantForm(forms.ModelForm):
     class Meta:
         model = models.PostShippingVariant
-        fields = ('shipping_first_name', 'shipping_last_name',
-                  'shipping_company_name', 'shipping_street_address_1',
-                  'shipping_street_address_2', 'shipping_city',
-                  'shipping_postal_code', 'shipping_country', 'shipping_phone')
+        exclude = ('delivery_group', 'name', 'description', 'price')

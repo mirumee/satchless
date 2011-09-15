37317c53cdeb923e9c3985c5f7b9a573236cde18
Brook Elgie <specialbrew@gmail.com>
2011-09-15 20:29:35 +0100

    A templatetag to get the Thumbnail object of an Image at the requested size

--- satchless/image/templatetags/satchless_image.py
+++ satchless/image/templatetags/satchless_image.py
@@ -1,7 +1,13 @@
 from django import template
 
+from ..models import Thumbnail
+
 register = template.Library()
 
 @register.filter
 def at_size(image, size):
     return image.get_absolute_url(size=size)
+
+@register.filter
+def img_at_size(image, size):
+    return Thumbnail.objects.get_or_create_at_size(image.id,size)

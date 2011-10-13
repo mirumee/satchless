96b97e9d6e342b60df17643758a563094d2944b5
Mirek Mencel <mirek@mirumee.com>
2011-10-13 16:27:53 +0200

    Demo shop: fix 'Thank you' view

--- examples/demo/core/views.py
+++ examples/demo/core/views.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
 
@@ -12,8 +12,8 @@ def home_page(request):
     messages.success(request, _(u'<strong>Welcome!</strong> This is a demo shop built on Satchless. Enjoy!'))
     return TemplateResponse(request, 'core/home_page.html')
 
-def thank_you_page(request, token):
-    order = order_app.get_order(request, token)
+def thank_you_page(request, order_token):
+    order = order_app.get_order(request, order_token)
     if not order.status in ('payment-failed', 'payment-complete', 'delivery'):
         return redirect('satchless-order-view', order_token=order.token)
     if order.status == 'payment-failed':

dab2b5666f3b9ba7d7c0befb421a20b983ddb85c
Patryk Zawadzki <patrys@pld-linux.org>
2011-10-22 17:42:30 +0200

    Pass currency code to field_name in pricing

--- satchless/contrib/pricing/field/__init__.py
+++ satchless/contrib/pricing/field/__init__.py
@@ -12,8 +12,10 @@ class ProductFieldGetter(FieldGetter, PricingHandler):
         price = kwargs.pop('price')
         if self.currency and self.currency != currency:
             return price
+        cur = currency.lower() if currency else ''
+        field_name = self.field_name % {'currency': cur}
         try:
-            instance_price = getattr(variant.product, self.field_name)
+            instance_price = getattr(variant.product, field_name)
         except AttributeError:
             return price
         return Price(instance_price, instance_price, currency=currency)
@@ -22,8 +24,10 @@ class ProductFieldGetter(FieldGetter, PricingHandler):
         price_range = kwargs.pop('price_range')
         if self.currency and self.currency != currency:
             return price_range
+        cur = currency.lower() if currency else ''
+        field_name = self.field_name % {'currency': cur}
         try:
-            instance_price = getattr(product, self.field_name)
+            instance_price = getattr(product, field_name)
         except AttributeError:
             return price_range
         min_price = Price(instance_price, instance_price, currency=currency)
@@ -36,8 +40,10 @@ class VariantFieldGetter(FieldGetter, PricingHandler):
         price = kwargs.pop('price')
         if self.currency and self.currency != currency:
             return price
+        cur = currency.lower() if currency else ''
+        field_name = self.field_name % {'currency': cur}
         try:
-            instance_price = getattr(variant, self.field_name)
+            instance_price = getattr(variant, field_name)
         except AttributeError:
             return price
         return Price(instance_price, instance_price, currency=currency)
@@ -46,9 +52,10 @@ class VariantFieldGetter(FieldGetter, PricingHandler):
         price_range = kwargs.pop('price_range')
         if self.currency and self.currency != currency:
             return price_range
-        field = self.field_name
-        minmax = product.variants.all().aggregate(min_price=Min(field),
-                                                  max_price=Max(field))
+        cur = currency.lower() if currency else ''
+        field_name = self.field_name % {'currency': cur}
+        minmax = product.variants.all().aggregate(min_price=Min(field_name),
+                                                  max_price=Max(field_name))
         min_price = Price(minmax['min_price'], minmax['min_price'],
                           currency=currency)
         max_price = Price(minmax['max_price'], minmax['max_price'],

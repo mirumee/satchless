93e6d4854c705502b9725243ef5f7d39aa621939
Tomasz Rybarczyk <paluho@gmail.com>
2011-11-04 00:47:18 +0100

    Variant form class as provider attribute in simplepost app

--- satchless/contrib/delivery/simplepost/__init__.py
+++ satchless/contrib/delivery/simplepost/__init__.py
@@ -5,6 +5,8 @@ from . import forms
 from . import models
 
 class PostDeliveryProvider(DeliveryProvider):
+    form_class = forms.PostShippingVariantForm
+
     def __unicode__(self):
         return ugettext("Post delivery")
 
@@ -17,9 +19,9 @@ class PostDeliveryProvider(DeliveryProvider):
         instance = models.PostShippingVariant(delivery_group=delivery_group,
                                               name=typ.name,
                                               price=typ.price)
-        return forms.PostShippingVariantForm(data or None, instance=instance,
-                                             prefix='delivery_group-%d' %
-                                             delivery_group.pk)
+        return self.form_class(data or None, instance=instance,
+                              prefix='delivery_group-%d' %
+                              delivery_group.pk)
 
     def create_variant(self, delivery_group, typ, form):
         return form.save()

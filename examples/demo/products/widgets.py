0613f84bc9d23adc59b8e03e1d51df41078ef03f
Tomasz Rybarczyk <paluho@gmail.com>
2012-02-09 00:53:13 +0100

    Demo shop: products admin fixes

--- examples/demo/products/widgets.py
+++ examples/demo/products/widgets.py
@@ -1,24 +1,17 @@
 # -*- coding:utf-8 -*-
 import os
 
-from django.conf import settings
-from django.contrib.admin.widgets import AdminFileWidget
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
-from django.conf import settings
 from django.core.urlresolvers import reverse
 import django.forms
-
-from django_images.settings import IMAGE_SIZES
-
-from . import models
 
 class AdminImageWidget(django.forms.FileInput):
     def render(self, name, value, attrs=None):
         output = ['<p class="file-upload">']
         if value and hasattr(value, 'instance'):
             image = value.instance
-            thumbnail_url = reverse('satchless-image-thumbnail', args=(image.id, 'admin'))
+            thumbnail_url = reverse('image-thumbnail', args=(image.id, 'admin'))
             output.append(u'<a class="image" rel="gallery" href="%s"><img src="%s" alt="" /></a><br />' % (
                     image.image.url, thumbnail_url))
         if value and hasattr(value, "url"):

86f0729e1acab32e1582ec758aa214a0b9769302
Jesús Espino <jesus.espino@kaleidos.net>
2012-03-01 11:06:47 +0100

    Enabling the use of StopPropagation for pricing handlers

--- satchless/pricing/handler.py
+++ satchless/pricing/handler.py
@@ -2,25 +2,31 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from satchless.core.handler import QueueHandler
 
-from . import PricingHandler
+from . import PricingHandler, StopPropagation
 
 
 class PricingQueue(PricingHandler, QueueHandler):
     def get_variant_price(self, variant, currency=None, price=None, quantity=1, **context):
         for handler in self.queue:
-            price = handler.get_variant_price(variant=variant,
-                                              currency=currency,
-                                              quantity=quantity,
-                                              price=price,
-                                              **context)
+            try:
+                price = handler.get_variant_price(variant=variant,
+                                                  currency=currency,
+                                                  quantity=quantity,
+                                                  price=price,
+                                                  **context)
+            except StopPropagation:
+                break
         return price
 
     def get_product_price_range(self, product, currency=None, price_range=None, **context):
         for handler in self.queue:
-            price_range = handler.get_product_price_range(product=product,
-                                                      currency=currency,
-                                                      price_range=price_range,
-                                                      **context)
+            try:
+                price_range = handler.get_product_price_range(product=product,
+                                                          currency=currency,
+                                                          price_range=price_range,
+                                                          **context)
+            except StopPropagation:
+                break
         return price_range
 
 

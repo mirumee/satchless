670820f9deb41b454f10669d949326295eee866e
Michal Ociepka <michal@ociepka.info>
2013-02-22 16:57:18 +0100

    Add new Cart and CartItem.

--- satchless/cart/__init__.py
+++ satchless/cart/__init__.py
@@ -1,3 +1,9 @@
+from decimal import Decimal
+from django.conf import settings
+from django.utils.translation import ugettext as _
+from satchless.item import ItemSet, ItemLine
+
+
 class InvalidQuantityException(Exception):
 
     def __init__(self, reason, quantity_delta):
@@ -6,4 +12,75 @@ class InvalidQuantityException(Exception):
 
     def __str__(self):
         return self.reason
+
+
+class CartItem(ItemLine):
+
+    def __init__(self, product, quantity):
+        self._product = product
+        self._quantity = quantity
+
+    def _get_product(self):
+        return self._product
+
+    def get_quantity(self):
+        return self._quantity
+
+    def get_price_per_item(self):
+        return self._product.price
+
+    product = property(_get_product)
+    quantity = property(get_quantity)
+
+
+class Cart(dict, ItemSet):
+
+    SESSION_KEY = 'cart'
+    modified = False
+
+    def __iter__(self):
+        for product, quantity in self.items():
+            yield CartItem(product, quantity)
+
+    def __getitem__(self, key):
+        return CartItem(key, super(Cart, self).__getitem__(key))
+
+    def __setitem__(self, key, val):
+        self.modified = True
+        return dict.__setitem__(self, key, val)
+
+    def __len__(self):
+        return sum([item.quantity for item in self])
+
+    def __unicode__(self):
+        return _('Cart (%(cart_count)s)' % {'cart_count': len(self)})
+
+    def check_quantity(self, product, quantity, replace=False):
+        '''
+        Method checks quantity value. Return valid quantity or raise
+        InvalidQuantityException.
+        '''
+        if not replace and product in self:
+            quantity += self[product].get_quantity()
+
+        if quantity < 0:
+            quantity = Decimal(0)
+
+        return quantity
+
+    def add_item(self, product, quantity, replace=False):
+        quantity = self.check_quantity(product, quantity, replace=replace)
+
+        if not quantity and product in self:
+            del self[product]
+        else:
+            self[product] = quantity
+
+        return self[product]
+
+    def replace_item(self, product, quantity):
+        return self.add_item(product, quantity, replace=True)
+
+    def get_default_currency(self, **kwargs):
+        return settings.SATCHLESS_DEFAULT_CURRENCY
 
--- satchless/cart/tests/__init__.py
+++ satchless/cart/tests/__init__.py
@@ -1,8 +1,49 @@
 from prices import Price
+from django.test import TestCase
+from satchless.item import ItemLine
 
+from ...cart import Cart
 from ...product.tests import product_app
 from ...util.models import construct
 from .. import models
+
+class Product(ItemLine):
+
+    def get_price_per_item(self, **kwargs):
+        return Price(1)
+
+class TestSessionCart(TestCase):
+
+    def setUp(self):
+        self.cart = Cart()
+        self.product = Product()
+
+    def test_add_item(self):
+        cart_item = self.cart.add_item(self.product, 1)
+        self.assertEqual(cart_item.get_quantity(), 1)
+        cart_item = self.cart.add_item(self.product, 2)
+        self.assertEqual(cart_item.get_quantity(), 3)
+
+        self.assertEqual((len(self.cart)), 3)
+
+    def test_replace_item(self):
+        cart_item = self.cart.replace_item(self.product, 10)
+        self.assertEqual(cart_item.get_quantity(), 10)
+
+        self.assertEqual((len(self.cart)), 10)
+
+    def test_check_quantity(self):
+
+        quantity = self.cart.check_quantity(self.product, 3)
+        self.assertEqual(quantity, 3)
+
+        quantity = self.cart.check_quantity(self.product, 0, replace=True)
+        self.assertEqual(quantity, 0)
+
+        quantity = self.cart.check_quantity(self.product, -3, replace=True)
+        self.assertEqual(quantity, 0)
+
+        self.assertEqual((len(self.cart)), 0)
 
 
 class TestCart(models.Cart):
@@ -17,4 +58,5 @@ from .app import AppTestCase
 from .magic_app import MagicAppTestCase, cart_app
 from .models import ModelsTestCase
 
-__all__ = ['AppTestCase', 'cart_app', 'MagicAppTestCase', 'ModelsTestCase']
+__all__ = ['AppTestCase', 'cart_app', 'MagicAppTestCase', 'ModelsTestCase',
+           'CartTest']

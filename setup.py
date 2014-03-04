490be7f73cd78eb1496dfc636d29d4342d8a6d3e
Patryk Zawadzki <patrys@room-303.com>
2014-03-04 15:28:34 +0100

    1.1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='hello@mirumee.com',
     description='An e-commerence framework',
     license='BSD',
-    version='1.1.1',
+    version='1.1.2',
     url='http://satchless.com/',
     packages=find_packages(),
     classifiers=CLASSIFIERS,
